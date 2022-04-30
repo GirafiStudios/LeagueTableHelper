3c5de8910dd204340d2290e75a36e661f92f46aa
GirafiStudios <faj10@msn.com>
2022-04-30 17:11:42 +0200

    Simplify a few things

--- src/main/java/com/girafi/LeagueTablesHelper/LeagueTablesHelper.java
+++ src/main/java/com/girafi/LeagueTablesHelper/LeagueTablesHelper.java
@@ -5,7 +5,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class LeagueTablesHelper {
 
@@ -47,34 +46,34 @@ public class LeagueTablesHelper {
         }
     }
 
-    private static Set<Map.Entry<String, List<String>>> sort(String fileName, boolean printResult) {
-        LinkedHashMap<String, List<String>> hashMap = new LinkedHashMap<>();
+    private static List<Map.Entry<String, List<Integer>>> sort(String fileName, boolean printResult) {
+        LinkedHashMap<String, List<Integer>> hashMap = new LinkedHashMap<>();
 
         for (String input : readInputFile(fileName)) {
             String[] data = input.split("\\t|((?!\\d)-(?=\\d))");
 
-            List<String> leagueEntries = new ArrayList<>();
-            leagueEntries.add(0, data[2]); //Played
-            leagueEntries.add(1, data[3]); //Wins
-            leagueEntries.add(2, data[4]); //Draws
-            leagueEntries.add(3, data[5]); //Loses
-            leagueEntries.add(4, data[6]); //Goals For
-            leagueEntries.add(5, data[7]); //Goals Against
-            leagueEntries.add(6, data[8]); //Points
+            List<Integer> leagueEntries = new ArrayList<>();
+            leagueEntries.add(0, Integer.parseInt(data[2])); //Played
+            leagueEntries.add(1, Integer.parseInt(data[3])); //Wins
+            leagueEntries.add(2, Integer.parseInt(data[4])); //Draws
+            leagueEntries.add(3, Integer.parseInt(data[5])); //Loses
+            leagueEntries.add(4, Integer.parseInt(data[6])); //Goals For
+            leagueEntries.add(5, Integer.parseInt(data[7])); //Goals Against
+            leagueEntries.add(6, Integer.parseInt(data[8])); //Points
             hashMap.put("replaceWithNewLine" + data[1], leagueEntries); //Team name, used as HashMap identifier
         }
 
-        Set<Map.Entry<String, List<String>>> entriesToBeSorted = hashMap.entrySet();
+        Set<Map.Entry<String, List<Integer>>> entriesToBeSorted = hashMap.entrySet();
 
-        Comparator<Map.Entry<String, List<String>>> comparator = (o1, o2) -> {
-            int points = Integer.parseInt(o1.getValue().get(6));
-            int points2 = Integer.parseInt(o2.getValue().get(6));
+        Comparator<Map.Entry<String, List<Integer>>> comparator = (o1, o2) -> {
+            int points = o1.getValue().get(6);
+            int points2 = o2.getValue().get(6);
 
             if (points == points2) {
-                int goalFor = Integer.parseInt(o1.getValue().get(4));
-                int goalAgainst = Integer.parseInt(o1.getValue().get(5));
-                int goalFor2 = Integer.parseInt(o2.getValue().get(4));
-                int goalAgainst2 = Integer.parseInt(o2.getValue().get(5));
+                int goalFor = o1.getValue().get(4);
+                int goalAgainst = o1.getValue().get(5);
+                int goalFor2 = o2.getValue().get(4);
+                int goalAgainst2 = o2.getValue().get(5);
                 int goalDifference = goalFor - goalAgainst;
                 int goalDifference2 = goalFor2 - goalAgainst2;
                 if (goalDifference == goalDifference2) {
@@ -87,83 +86,28 @@ public class LeagueTablesHelper {
             }
         };
 
-        List<Map.Entry<String, List<String>>> entries = new ArrayList<>(entriesToBeSorted);
+        List<Map.Entry<String, List<Integer>>> entries = new ArrayList<>(entriesToBeSorted);
         entries.sort(comparator);
-
-        LinkedHashMap<String, List<String>> sortedByValue = new LinkedHashMap<>(entries.size());
-        for (Map.Entry<String, List<String>> entry : entries) {
-            sortedByValue.put(entry.getKey(), entry.getValue());
-        }
-        Set<Map.Entry<String, List<String>>> entrySetSortedByValue = sortedByValue.entrySet();
-
 
         if (printResult) {
             int placing = 0;
-            for (Map.Entry<String, List<String>> entry : entrySetSortedByValue) {
+            for (Map.Entry<String, List<Integer>> entry : entries) {
                 String s = entry.getKey();
                 placing++;
                 System.out.print("\t" + replaceUnwantedCharacters(s.replace("replaceWithNewLine", "\n") + "\t\t\t\t" + placing + hashMap.get(s).stream().map(m -> "\t" + m).collect(Collectors.toList())));
             }
             System.out.println();
         }
-        return entrySetSortedByValue;
+        return entries;
     }
 
     public static void merge() {
-        Set<Map.Entry<String, List<String>>> fallStage = sort("Input.txt", false);
-        HashMap<String, List<Integer>> fallStageMap = new HashMap<>();
-        Set<Map.Entry<String, List<String>>> promotionStage = sort("MergePromotion.txt", false);
-        HashMap<String, List<Integer>> promotionStageMap = new HashMap<>();
-        Set<Map.Entry<String, List<String>>> relegationStage = sort("MergeRelegation.txt", false);
-        Set<Map.Entry<String, List<String>>> combined = new HashSet<>();
+        List<Map.Entry<String, List<Integer>>> fallStage = sort("Input.txt", false);
+        List<Map.Entry<String, List<Integer>>> promotionStage = sort("MergePromotion.txt", false);
+        List<Map.Entry<String, List<Integer>>> relegationStage = sort("MergeRelegation.txt", false);
+        List<Map.Entry<String, List<String>>> combined = new ArrayList<>();
 
-        for (Map.Entry<String, List<String>> fallEntry : fallStage) {
-            fallStageMap.put(fallEntry.getKey(), fallEntry.getValue().stream().map(Integer::parseInt).collect(Collectors.toList()));
-        }
-        for (Map.Entry<String, List<String>> promotionEntry : promotionStage) {
-            promotionStageMap.put(promotionEntry.getKey(), promotionEntry.getValue().stream().map(Integer::parseInt).collect(Collectors.toList()));
-        }
-
-        for (String fallString : fallStageMap.keySet()) {
-            if (promotionStageMap.containsKey(fallString)) {
-                for (String promotionString : promotionStageMap.keySet()) {
-                    List<Integer> fallResults = fallStageMap.get(promotionString);
-                    List<Integer> promotionResults = promotionStageMap.get(promotionString);
-                    List<Integer> result = IntStream.range(0, fallStageMap.values().size())
-                            .mapToObj(i -> fallResults.get(i) + promotionResults.get(i))
-                            .collect(Collectors.toList());
-                    System.out.println(result);
-                }
-            }
-        }
-
-
-
-
-        //TEMPORARY OUTPUTS JUST FOR REFERENCE
-        int placing = 0;
-        for (Map.Entry<String, List<String>> entry : fallStage) {
-            String s = entry.getKey();
-            placing++;
-            System.out.print("\t" + replaceUnwantedCharacters(s.replace("replaceWithNewLine", "\n") + "\t\t\t\t" + placing + entry.getValue().stream().map(m -> "\t" + m).collect(Collectors.toList())));
-        }
-        System.out.println();
-
-        int placing1 = 0;
-        for (Map.Entry<String, List<String>> entry : promotionStage) {
-            String s = entry.getKey();
-            placing1++;
-            System.out.print("\t" + replaceUnwantedCharacters(s.replace("replaceWithNewLine", "\n") + "\t\t\t\t" + placing1 + entry.getValue().stream().map(m -> "\t" + m).collect(Collectors.toList())));
-        }
-        System.out.println();
-
-        int placing2 = 0;
-        for (Map.Entry<String, List<String>> entry : relegationStage) {
-            String s = entry.getKey();
-            placing2++;
-            System.out.print("\t" + replaceUnwantedCharacters(s.replace("replaceWithNewLine", "\n") + "\t\t\t\t" + placing2 + entry.getValue().stream().map(m -> "\t" + m).collect(Collectors.toList())));
-        }
-        System.out.println();
+        
     }
 
     public static List<String> readInputFile(String fileName) {
